a2689665a42c5b69dd6b733a6258db58899961fe
Meisze Yau <meiszey@gmail.com>
2020-04-08 22:56:31 -0400

    Fixed issues with link missing from fields that point to Types with the same name (e.g. info field points to Info type)

--- controllers.js
+++ controllers.js
@@ -5,6 +5,7 @@ const { printSchema } = require ('graphql');
 const fs = require('fs');
 const path = require('path');
 const graphURL = "https://worldcup-graphql.now.sh/";
+// const graphURL = "https://polaris.shopify.com/api";
 const fetch = require('node-fetch');
 
 const controller = {};
@@ -22,13 +23,27 @@ controller.getSchema = (req, res, next) => {
   })
     .then(res => res.json())
     .then(data => {
-      res.locals.schema = JSON.stringify(data, null, 2); // put " data cleaning" function in 2nd parameter of stringify
+      res.locals.schema = JSON.stringify(data, null, 2); 
+      fs.writeFileSync(path.resolve(__dirname, 'schema.json'), res.locals.schema);
     })
     .then(() => next());
 };
 
 // converts schema into an object of Types and their respective fields (along with references to other Types)
 // output is in format e.g.:
+
+controller.convertSchema = (req, res, next) => {
+  const sourceSchema = JSON.parse(res.locals.schema);
+  const cleanedSchema = cleanSchema(sourceSchema);
+  const d3Json = schemaToD3(cleanedSchema);
+  // Writes and saves the JSON file into root folder
+  fs.writeFileSync(path.resolve(__dirname, 'd3schema.json'), JSON.stringify(d3Json, null, 2));
+  // Stores the file path for future middleware to access to implement in d3
+  res.locals.path = path.resolve(__dirname, 'd3schema.json');
+  return next();
+};
+
+// cleanSchema cleans the schema received from the client's graphQL endpoint into format:
 /*
 {
   Query: [
@@ -41,20 +56,7 @@ controller.getSchema = (req, res, next) => {
   ....
   Result: [ 'goalsHomeTeam', 'goalsAwayTeam' ]
 }
-
 */
-controller.convertSchema = (req, res, next) => {
-  const sourceSchema = JSON.parse(res.locals.schema);
-  const cleanedSchema = cleanSchema(sourceSchema);
-  const d3Json = schemaToD3(cleanedSchema);
-  // Writes and saves the JSON file into root folder
-  fs.writeFileSync(path.resolve(__dirname, 'd3schema.json'), JSON.stringify(d3Json, null, 2));
-  // Stores the file path for future middleware to access to implement in d3
-  res.locals.path = path.resolve(__dirname, 'd3schema.json');
-  console.log(d3Json);
-  return next();
-};
-
 function cleanSchema(sourceSchema) {
   const schemaTypes = sourceSchema.data.__schema.types;
   const types = {};
@@ -66,8 +68,12 @@ function cleanSchema(sourceSchema) {
       for (let j = 0; j < schemaTypes[i].fields.length; j++) {
         if (schemaTypes[i].fields[j].name && !schemaTypes[i].fields[j].isDeprecated) {
           // checks if the type of a field references another Type 
-          if (schemaTypes[i].fields[j].type.ofType && schemaTypes[i].fields[j].type.ofType.ofType) {
-            // creates a key-value pair of relationship between the field name and the type if it points to another type
+          if (schemaTypes[i].fields[j].type.kind === 'OBJECT') {
+            const fieldsLink = {};
+            fieldsLink[schemaTypes[i].fields[j].name] = schemaTypes[i].fields[j].type.name;
+            fieldsList.push(fieldsLink);
+          } else if (schemaTypes[i].fields[j].type.ofType && schemaTypes[i].fields[j].type.ofType.ofType) {
+            // creates a key-value pair of relationship between the field name and the Type if it points to another Type
             const fieldsLink = {};
             fieldsLink[schemaTypes[i].fields[j].name] = schemaTypes[i].fields[j].type.ofType.ofType.name;
             fieldsList.push(fieldsLink);
@@ -82,10 +88,29 @@ function cleanSchema(sourceSchema) {
   return types;
 }
 
+// schemaToD3 converts the "cleaned" schema into a JSON file for d3 in format:
+
+/*
+{
+  nodes: [
+    { name: 'Query', type: 'Type' },
+    { name: 'info', type: 'field' },
+    ....
+  ],
+  links: [
+    { source: 'info', target: 'Info' },
+    { source: 'Query', target: 'info' },
+    ...
+  ]
+}
+
+*/
+
 function schemaToD3(cleanedSchema) {
   const d3Json = {};
   const nodesArray = [];
   const linksArray = [];
+  const typesArray = Object.keys(cleanedSchema);
   // eslint-disable-next-line no-use-before-define
   for (let key in cleanedSchema) {
     const node = {};
@@ -93,18 +118,21 @@ function schemaToD3(cleanedSchema) {
     node.type = 'Type';
     nodesArray.push(node);
     for (let i = 0; i < cleanedSchema[key].length; i++) {
+      // create nodes for fields that do not return other Types
+      const fieldName = Object.keys(cleanedSchema[key][i]);
       if (typeof cleanedSchema[key][i] !== 'object') {
         const node = {};
         node.name = cleanedSchema[key][i];
         node.type = 'field';
         nodesArray.push(node);
+        // create links from each Type to their fields
         const link = {};
         link.source = key;
         link.target = cleanedSchema[key][i];
         linksArray.push(link);
-      } else {
+      } 
+      else {
         const node = {};
-        const fieldName = Object.keys(cleanedSchema[key][i]);
         node.name = fieldName[0];
         node.type = 'field';
         nodesArray.push(node);
